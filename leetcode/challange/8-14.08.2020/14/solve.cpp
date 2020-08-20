2dcb701c922c5658442533beb57b1801c1519b70
withelm <withelm@gmail.com>
2020-08-20 20:00:02 +0200

    20.08.2020

---
+++ leetcode/challange/8-14.08.2020/14/solve.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int datas[255 + 10];
+    int longestPalindrome(string s) {
+        int result = 0;
+        for(int i = 0; i < 255 + 10; i++) {
+            datas[i] = 0;
+        }
+        for(int i = 0; i < s.size(); i++) {
+            
+            ++datas[s[i]];
+        }
+        
+        bool isEven = false;
+        for(int i = 0; i < 255 + 10; i++) {
+            
+            if (datas[i] % 2 == 1) isEven = true;
+            result += (datas[i] / 2) * 2;
+        }
+        return result + isEven;
+    }
+};
+
+
+int main()
+{
+ 
+}
